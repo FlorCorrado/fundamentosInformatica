fab3e94e18b70bbaec70a5c65c87dc0076f85398
Florencia Corrado <florencia.corrado@mercadolibre.com>
2019-08-08 20:01:22 -0300

    ejercicio 6 tp 2

---
+++ trabajoPractico2/tp2Ejercicio6.py
@@ -0,0 +1,6 @@
+a = int(input("Ingrese el primer número: "))
+b = int(input("Ingrese el segundo número: "))
+c = int(input("Ingrese el segundo número: "))
+
+print("El promedio de los números ingresados es igual a:", (a + b + c) / 3)
+
