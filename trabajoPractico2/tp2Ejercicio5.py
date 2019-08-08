f71cbc4b29c1543095d107abc730c3b21570c885
Florencia Corrado <florencia.corrado@mercadolibre.com>
2019-08-08 19:57:19 -0300

    tp2 ejercicio 5

---
+++ trabajoPractico2/tp2Ejercicio5.py
@@ -0,0 +1,9 @@
+metros = int(input("Ingrese una medida en metros: "))
+
+print("La medida en centímetros es igual a:", metros * 100, "cm")
+
+print("La medida en pulgadas es igual a:", metros * 100 / 2.54, "pulgadas")
+
+print("La medida en pies es igual a:",  metros * 100 / 2.54 / 12, "cm")
+
+print("La medida en yardas es igual a:",  metros * 100 / 2.54 / 12 / 3, "cm")
