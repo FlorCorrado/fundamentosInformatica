9a4353985c808b20a91f561bb22ab3392a80e81c
Florencia Corrado <florencia.corrado@mercadolibre.com>
2019-08-08 22:01:37 -0300

    tp 2 ejercicio 10

---
+++ trabajoPractico2/tp2Ejercicio10.py
@@ -0,0 +1,5 @@
+gradosCentigrados = int(input("Ingrese una temperatura en grados centigrados: "))
+
+gradosFahrenheit = gradosCentigrados / (5/9) + 32
+
+print("La temperatura en grados Fahrenheit es:", gradosFahrenheit)
