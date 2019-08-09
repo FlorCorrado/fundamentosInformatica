91ad50f0369d3462817284b8365a980a051848e7
Florencia Corrado <florencia.corrado@mercadolibre.com>
2019-08-09 14:38:26 -0300

    con segunda solucion

--- trabajoPractico2/tp2Ejercicio11.py
+++ trabajoPractico2/tp2Ejercicio11.py
@@ -2,4 +2,12 @@ valorBinario = input("Ingrese un número binario de 4 dígitos: ")
 
 numDecimal = int(valorBinario, 2)
 
+numero1 = int(valorBinario[0])
+numero2 = int(valorBinario[1])
+numero3 = int(valorBinario[2])
+numero4 = int(valorBinario[3])
+
+valorFinal = 2**3 * numero1 + 2**2 * numero2 + 2**1 * numero3 + 2**0 * numero1
+
 print("El valor binario ingresado en decimal equivale a ", numDecimal)
+print("El valor binario ingresado en decimal equivale a ", valorFinal)
