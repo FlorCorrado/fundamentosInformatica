08141aef579e7f27dbda3425b32ab91313c6ae21
Florencia Corrado <florencia.corrado@mercadolibre.com>
2019-08-11 14:43:37 -0300

    tp3 ej 4

---
+++ trabajoPractico3/tp3ej4.py
@@ -0,0 +1,10 @@
+num1 = int(input("Ingrese un número: "))
+
+num2 = int(input("Ingrese otro número: "))
+
+if num1 % num2 == 0:
+    print("El número", num1, "es múltiplo de", num2)
+elif num2 % num1 == 0:
+    print("El número", num2, "es múltiplo de", num1)
+else:
+    print("Los numeros no son multiplos")
