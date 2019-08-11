543e88138bfeaf349821db6d7c3d1fc6c7be6edb
Florencia Corrado <florencia.corrado@mercadolibre.com>
2019-08-11 14:22:43 -0300

    tp3 ejercicio 1

---
+++ trabajoPractico3/tp3Ej1.py
@@ -0,0 +1,8 @@
+num1 = int(input("Por favor ingrese un número: "))
+
+num2 = int(input("Por favor ingrese otro número: "))
+
+if num1 > num2:
+    print("El número", num1, "es el mayor")
+else:
+    print("El número", num2, "es el mayor")
